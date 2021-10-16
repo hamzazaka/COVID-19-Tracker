50280df30355607b0b167318b8f3a8c666f53e1d
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-16 11:15:37 -0400

    fetching data form the api

--- covid-app/src/components/FetchData.js
+++ covid-app/src/components/FetchData.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import axios from 'axios';
 
 export default function FetchData() {
+
+    const [countries,setCountries]=useState([]);
+
+    const URL='https://api.covid19api.com/summary';
+
+    const fetchCountryData=  async ()=>{
+        const response=await axios.get(URL);
+        console.log(response);
+
+    }
+
+
+    useEffect(()=>{
+        fetchCountryData()
+    })
+
     return (
         <div>
-            
+            <h1>hello form fetch Data</h1>
         </div>
     )
 }
