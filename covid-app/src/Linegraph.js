c22740d930179eddf4304813e25f09bbacd07f3a
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-18 18:12:20 -0400

    still owrking on the linegraph

--- covid-app/src/Linegraph.js
+++ covid-app/src/Linegraph.js
@@ -1,8 +1,71 @@
 import React, { useState,useEffect } from 'react';
-import {Line} from 'react-chartjs-2'
+import {Line} from 'react-chartjs-2';
+import numeral from 'numeral';
+
+const options={
+  legend:{
+      display:false,
+  },
+  elements:{
+      point:{
+          radius:0,
+      },
+  },
+  maintainAspectRatio:false,
+  tooltips:{
+    mode:"index",
+    intersect:false,
+    callbacks:{
+        label:function(tooltipItem,data){
+            return numeral(tooltipItem.value).format("+0,0")
+        }
+    }
+  },
+  scales:{
+      xAxes:[
+          {
+              type:"time",
+              time:{
+                  format:"MM/DD/YY",
+                  tooltipFormat:"ll",
+              },
+          },
+        ],
+      yAxes:[
+          {
+              gridlines:{
+                  display:false,
+              },
+              ticks:{
+                  callback:function(value,index,values){
+                      return numeral(value).format("0o")
+                  },
+              }
+          }
+      ]
+  },
+},
 
 export default function Linegraph() {
     const [data,setData]=useState({});
+
+
+      const buildChartData=(data,casesType='cases')=>{
+        const chartData=[];
+        let lastDataPoint;
+
+        for(let date in data.cases){
+            if(lastDataPoint){
+                const newDataPoint={
+                    x:date,
+                    y:data[casesType][date]-lastDataPoint
+                }
+                chartData.push(newDataPoint)
+            }
+            lastDataPoint=data[casesType][date];
+        };
+        return chartData;
+    }
 
 
     useEffect(() => {
@@ -10,15 +73,12 @@ export default function Linegraph() {
         .then(response=>response.json())
         .then(data=>{
             console.log(data);
+            let chartData=buildChartData(data,'cases');
+            setData(chartData)
         })
     }, [])
 
-    const buildChartData=(data)=>{
-        const chartData=[];
-        let lastDataPoint;
-
-        data.cases
-    }
+  
 
 
 
@@ -26,10 +86,17 @@ export default function Linegraph() {
     return (
         <div>
             <h1>I am a graph</h1>
-            {/* <Line
-            data
-            options
-            /> */}
+            <Line
+            options={options}
+            data={{
+                datasets:[{
+                    backgroundColor:"rgba(204,16,0.7)",
+                    borderColor:"#CC1034",
+                    data:data
+                }
+                ]
+            }}
+            />
         </div>
     )
 }
