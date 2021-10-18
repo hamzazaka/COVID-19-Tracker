908d264ee9da5343c18608d24648ed22e8e35127
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-18 11:15:27 -0400

    completely redesigning the whole stuff

--- covid-app/src/components/header.js
+++ covid-app/src/components/header.js
@@ -5,11 +5,31 @@ export default function Header({countries}) {
     console.log(countries);
 
     return (
-        <div>
-            <h1>Covid-19 Tracker</h1>
-            <li> New Cases {countries.data.Global.NewConfirmed}</li>
-            <li>Total Cases {countries.data.Global.TotalConfirmed}</li>
-            <li>New Deaths {countries.data.Global.TotalConfirmed}</li><li>Total Dealths {countries.data.Global.TotalConfirmed}</li>
-        </div>
+        <div className='header'> 
+            <h1 className='text-center'>Covid-19 Tracker</h1>
+
+          <div className=' header-img row text-center'>
+
+            <div className="col-md-6 ">
+             <h2>New Cases</h2>
+             <li>{countries.data.Global.NewConfirmed}</li>
+            </div>
+             <div className="col-md-6 ">
+            <h2>Total Cases</h2>
+            <li>{countries.data.Global.TotalConfirmed}</li>
+            </div>
+
+             <div className="col-md-6 ">
+                <h2>New Deaths</h2>
+                <li>{countries.data.Global.NewDeaths}</li>
+             </div> 
+            
+            <div className="col-md-6 ">
+              <h2>Total Deaths</h2>
+              <li>{countries.data.Global.TotalDeaths}</li>
+            </div>
+
+            </div>
+         </div>
     )
 }
